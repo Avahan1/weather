d6c3d2a54947bf31acb3e9dee03bbce31a9853a6
AvaHan <h-han@kellogg.northwestern.edu>
2016-01-24 18:42:48 -0600

    first try current weather

--- weather.js
+++ weather.js
@@ -2,12 +2,33 @@ $(function() {
   var handleWeatherResponse = function(data) {
     console.log(data);
 
-    // Put your code here to change the "markup" variable. 
+    // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
-    
-    var markup = "The weather report... " + "will be here when I finish my homework.";
-    
-    
+
+    var markup = "The weather report... " + "will be here when I finish my homework for the programming class.";
+
+    var current={latitude: 42.056459, longitude: -87.675267, timezone: "America/Chicago", offset: -6,
+      currently: {apparentTemperature: 29.16
+        cloudCover: 0.93
+        dewPoint: 24.63
+        humidity: 0.83
+        icon: "partly-cloudy-night"
+        nearestStormBearing: 99
+        nearestStormDistance: 18
+        ozone: 322.4
+        precipIntensity: 0
+        precipProbability: 0
+        pressure: 1015.74
+        summary: "Mostly Cloudy"
+        temperature: 29.16
+        time: 1453680824
+        visibility: 8.26
+        windBearing: 98
+        windSpeed: 2.45}
+      };
+
+
+
     // End of your code
 
     $('.weather-report').html(markup);
@@ -23,3 +44,4 @@ $(function() {
     });
   });
 });
+//
